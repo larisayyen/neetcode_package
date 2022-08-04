8be5768eb5d1d9bb7819dc56f7c7f6899390a5a1
yanyi951014 <10889949+yanyi951014@user.noreply.gitee.com>
2022-08-04 21:02:56 +0800

    update two_D.py

--- neetcode_package/two_D.py
+++ neetcode_package/two_D.py
@@ -1,4 +1,7 @@
 
+from this import d
+
+
 def uniquePaths(m,n):
     '''
     from top-left corner to bottom-right corner
@@ -16,3 +19,18 @@ def uniquePaths(m,n):
         row = newrow
 
     return row[0]
+
+def longestCommonSubsequence(text1, text2):
+    '''
+    solve sub problem
+    '''
+    dp = [[0 for j in range(len(text2 + 1))] for i in range(len(text1)+1)]
+
+    for i in range(len(text1) - 1,-1,-1):
+        for j in range(len(text2) -1,-1,-1):
+            if text1[i] == text2[j]:
+                dp[i][j] = 1 + dp[i+1][j+1]
+            else:
+                dp[i][j] = max(dp[i][j+1],dp[i+1][j])
+
+    return dp[0][0]
